d930c979dc33acb341ac57c29452da5254b4fabb
Lrop <Lagropner04@gmail.com>
2019-06-24 13:09:17 -0600

    firebase config

---
+++ assets/train.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCUAOsFYIHtxoQfgOls8cx-vXps-4kdh6U",
+    authDomain: "train-time-database.firebaseapp.com",
+    databaseURL: "https://train-time-database.firebaseio.com",
+    projectId: "train-time-database",
+    storageBucket: "",
+    messagingSenderId: "329729684767",
+    appId: "1:329729684767:web:214806464be10a5a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
