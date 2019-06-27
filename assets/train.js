12fefc7e723c6e6739b99d267aaca53a1264fa17
Lrop <Lagropner04@gmail.com>
2019-06-27 12:38:29 -0600

    JS fix

--- assets/train.js
+++ assets/train.js
@@ -1,19 +1,19 @@
- var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyCUAOsFYIHtxoQfgOls8cx-vXps-4kdh6U",
     authDomain: "train-time-database.firebaseapp.com",
     databaseURL: "https://train-time-database.firebaseio.com",
     projectId: "train-time-database",
-    storageBucket: "",
+    storageBucket: "train-time-database.appspot.com",
     messagingSenderId: "329729684767",
     appId: "1:329729684767:web:214806464be10a5a"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
-
+  console.log(firebase);
   var trainData = firebase.database();
+  
 
-  $("#button-submit").on("click", function(evnt){
+  $("#button-submit").on("click", function(event){
         event.preventDefault();
 
 
@@ -41,7 +41,18 @@
 
         trainData.ref().push(newTrain);
 
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.firstTrain);
+        console.log(newTrain.frequency);
 
+
+        alert("trian added bitch");
+
+        $('#train-name').val("");
+        $('#destination').val("");
+        $('#firstTrain').val("");
+        $('#frequency').val("");
 
   });
-  
+
